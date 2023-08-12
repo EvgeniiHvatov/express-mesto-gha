fdc56e162406a7d49162ceb4663d9e11cf388c79
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-08-12 21:42:48 +0300

    c

--- controllers/users.js
+++ controllers/users.js
@@ -89,7 +89,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: true,
       });
-      res.status(200).send({ token, message: 'Регистрация прошла успешно!' });
+      res.send({ token, message: 'Регистрация прошла успешно!' });
     })
     .catch(next);
 };
